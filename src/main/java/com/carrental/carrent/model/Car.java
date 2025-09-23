5345367f2a073d31e8e6456f35432d012d0e2318
Egor <llikojia162@gmail.com>
2025-09-23 19:33:19 +0200

    - Added Maven Checkstyle plugin to pom.xml - Added GitHub Actions CI workflow - Created Liquibase changelog file - Added .env.sample for environment variables - Integrated Swagger for API documentation - Added initial README.md

---
+++ src/main/java/com/carrental/carrent/model/Car.java
@@ -0,0 +1,45 @@
+package com.carrental.carrent.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE cars SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String model;
+    @Column(nullable = false)
+    private String brand;
+    @Column(nullable = false)
+    private CarType carType;
+    @Column(nullable = false)
+    private int inventory;
+    @Column(nullable = false)
+    private BigDecimal dailyFee;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+}
+
+enum CarType {
+    SEDAN,
+    SUV,
+    HATCHBACK,
+    UNIVERSAL
+}
---
+++ src/main/java/com/carrental/carrent/model/Payment.java
@@ -0,0 +1,53 @@
+package com.carrental.carrent.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.boot.web.servlet.server.Session;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE payments SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Status status;
+    @Column(nullable = false)
+    private PaymentType paymentType;
+    @Column(nullable = false)
+    private Long rentalId;
+    @Transient
+    @Column(nullable = false)
+    private Session sessionUrl;
+    @Transient
+    @Column(nullable = false)
+    private Session sessionId;
+    @Column(nullable = false)
+    private BigDecimal amountToPay;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
+
+enum Status {
+    PENDING,
+    PAID
+}
+
+enum PaymentType {
+    PAYMENT,
+    FINE
+}
